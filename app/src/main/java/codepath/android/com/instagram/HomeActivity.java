3fd08a036ead34574026c7d9264c2134959bd8b5
calren <calren24@gmail.com>
2015-08-02 19:42:36 -0700

    First commit with skeleton code

---
+++ app/src/main/java/codepath/android/com/instagram/HomeActivity.java
@@ -0,0 +1,31 @@
+package codepath.android.com.instagram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    private void setUpBottomTabBar() {
+
+    }
+
+    private void showProfileActivity() {
+
+    }
+
+    private void showHomeActivity() {
+
+    }
+
+    private void launchCamera() {
+        
+    }
+}
